ef777635a4821413941e97365d43d632e860d0f6
David SeWell <davidsewell3145@gmail.com>
2019-10-13 22:17:50 -0700

    DS - kernel perceptron seems to be working

---
+++ KernelMethods/Basic/basic_kernel_classifier.py
@@ -0,0 +1,144 @@
+################################################
+# Module for running some basic
+# binary classifiers using different kernel methods
+################################################
+import numpy as np
+from sklearn import datasets
+
+
+##########################
+# Define kernels to use
+##########################
+def kernel_k1(x,y):
+    return np.dot(x,y)
+
+def kernel_k3(x,y):
+    return ( (1+ np.dot(x,y) )**2)
+
+def run_binary_iris_classifier(iris_data_x,iris_data_y,train_data_indexes,test_data_x,test_data_y):
+    '''
+    Example of running binary classifier on iris data set
+    :return:
+    '''
+
+
+    ###########################
+    # Create Classifier
+    ###########################
+    positive_indexes = [i for i in train_data_indexes if iris_data_y[i] == 1]
+    negative_indexes = [i for i in train_data_indexes if iris_data_y[i] != 1]
+    x_p = iris_data_x[positive_indexes]
+    x_m = iris_data_x[negative_indexes]
+
+    # calculate center of positive and negative vectors and get our offset constant b
+    # b = 1/2
+    k_pp = np.zeros((len(x_p), len(x_p)))
+    k_mm = np.zeros((len(x_m), len(x_m)))
+
+    for i in range(k_pp.shape[0]):
+        for j in range(k_pp.shape[1]):
+            k_pp[i][j] = np.dot(x_p[i], x_p[j])
+
+    for i in range(k_mm.shape[0]):
+        for j in range(k_mm.shape[1]):
+            k_mm[i][j] = np.dot(x_m[i], x_m[j])
+
+    b = (k_mm.sum() / len(k_mm) ** 2 - k_pp.sum() / len(k_pp) ** 2)
+    b = b / 2.0
+
+    data_x = np.concatenate((x_p, x_m))  # combine positive and negative vectors together
+
+    alpha = [1.0 / len(x_p) for _ in range(len(x_p))] + [-1 / len(x_m) for _ in range(len(x_m))]
+
+    ###################################
+    # Evaluate the kernel on test set
+    ###################################
+    num_right = 0
+    num_wrong = 0
+    for i in range(test_data_x.shape[0]):
+
+        x_test = test_data_x[i]
+        y_test = test_data_y[i]
+        k_xx = 0
+        for j in range(data_x.shape[0]):  # loop over all points in data set
+            k_xx += alpha[j] * kernel_k1(data_x[j], x_test)
+
+        y_hat = k_xx + b
+        # check result vs actual
+        if y_hat * y_test > 0:
+            num_right += 1
+        else:
+            num_wrong += 1
+
+    print('Number right = {}'.format(num_right))
+    print('Number wrong = {}'.format(num_wrong))
+    print('Percent correct = {}'.format(float(num_right) / (num_right + num_wrong)))
+
+
+if __name__ == '__main__':
+
+
+    binary_iris_classifier = 1
+    if binary_iris_classifier:
+
+        iris_data = datasets.load_iris()
+
+        ###########################################################################
+        # the dataset contains a set of 150 records under five attributes -
+        # X = petal length, petal width, sepal length, sepal width
+        # Y (target) = species (setosa,versicolor,virginica )
+        ###########################################################################
+
+        iris_data_x = iris_data.data
+        iris_data_y = iris_data.target
+
+        # first start with setosa versus versicolor
+        print('Running kernel classifier on Setosa vs Versicolor')
+
+        # Random sample 80 data points for training
+        # From the first 100 points in the data set grab 80 at random and without replacement
+        train_data_indexes = np.random.choice(100, 80, replace=False)
+        test_data_indexes = [i for i in range(100) if i not in train_data_indexes]  # get whats left over for testing
+
+        train_data_x = iris_data_x[train_data_indexes]
+        train_data_y = iris_data_y[train_data_indexes]
+
+        test_data_x = iris_data_x[test_data_indexes]
+        test_data_y = iris_data_y[test_data_indexes]
+
+        # in keeping with the form of this simple binary classifier
+        # I will break the data into labels with {1,-1}
+        # I replace y labels with 0 to be -1
+        train_data_y[np.where(train_data_y == 0)] = -1
+        test_data_y[np.where(test_data_y == 0)] = -1
+
+        run_binary_iris_classifier(iris_data_x, iris_data_y, train_data_indexes, test_data_x, test_data_y)
+
+
+        # second run on Veriscolor vs virginica
+        print('Running kernel classifier on Veriscolor vs virginica')
+
+        #############################################
+        # Run same code below for Veriscolor vs virginica
+        #############################################
+
+        # Random sample 80 data points for training
+        # From the last 100 in the data set grab 80 at random and without replacement
+        possible_indexes = [i for i in range(50, 150)]
+        train_data_indexes = np.random.choice(possible_indexes, 80, replace=False)
+        test_data_indexes = [i for i in range(50, 150) if
+                             i not in train_data_indexes]  # get whats left over for testing
+
+        train_data_x = iris_data_x[train_data_indexes]
+        train_data_y = iris_data_y[train_data_indexes]
+
+        test_data_x = iris_data_x[test_data_indexes]
+        test_data_y = iris_data_y[test_data_indexes]
+
+        # in keeping with the form of this simple binary classifier
+        # I will break the data into labels with {1,-1}
+        # I replace y labels with 0 to be -1
+        train_data_y[np.where(train_data_y == 2)] = -1  # treat virginica as the negative example
+        test_data_y[np.where(test_data_y == 2)] = -1
+
+        run_binary_iris_classifier(iris_data_x, iris_data_y, train_data_indexes, test_data_x, test_data_y)
---
+++ KernelMethods/Basic/kernelized_perceptron.py
@@ -0,0 +1,182 @@
+import matplotlib.pyplot as plt
+
+from numpy.random import multivariate_normal
+import numpy as np
+
+
+def get_circle(radius, num_points=100):
+    x_points = np.linspace(-radius, radius, num_points)
+    circle_points_y = []
+    circle_points_x = []
+    for x_i in x_points:
+        y_i = np.sqrt(radius ** 2 - x_i ** 2)
+        circle_points_x.append(x_i)
+        circle_points_x.append(x_i)
+        circle_points_y.append(y_i)
+        circle_points_y.append(-1.0 * y_i)
+
+    return circle_points_y, circle_points_x
+
+def kernel_k1(x,y):
+    return np.dot(x,y)
+
+def kernel_k2(x,y,p=2):
+
+    k1 = kernel_k1(x,y)
+
+    k2 = (1 + k1)**p
+    return k2
+
+def phi_I(x):
+    return x
+
+def phi_p(x):
+
+    phi_x = [x[0]**2,x[1]**2,x[0]**2 + x[1]**2]
+    return np.array(phi_x)
+
+def run_kernalized_perceptron(X ,Y,kernel=kernel_k1,phi=phi_I):
+
+    ####################
+    # Construct kernel
+    ####################
+    # init to zero
+    alphas = np.array([0 for _ in range(X.shape[0])])
+    b = 0
+
+    for iter in range(5):
+
+        print('Running iteration {}'.format(iter))
+
+        for i in range(X.shape[0]):
+
+            x = X[i]
+
+            #x = phi(x)
+
+            k_mm = 0
+            for j in range(X.shape[0]):
+
+                # k_mm += alphas[j]*np.dot(X[j],x)
+                k_mm += alphas[j] * kernel(X[j] ,x)
+
+            y_hat = k_mm
+
+            if (y_hat *Y[i] < 0) or i == 0: # wrong
+
+                # update every alpha by k(x,y)
+                #k_update = 0
+                #for k in range(X.shape[0]):
+
+                    # update = np.dot(Y[i]*np.ones(x.shape),x)
+                    #k_update += np.dot(X[k] ,X_new[i] ) *Y[i]
+
+                #alphas = alphas + (X_new[i]*Y[i]).sum()
+                alphas[i] = alphas[i] + Y[i]
+                #alphas = alphas + Y
+                b += k_mm
+
+    return alphas
+
+def kernel_perceptron_sanity_check(X_new, X, Y, alphas,kernel,phi):
+    num_wrong = 0
+    num_right = 0
+
+    for i in range(X_new.shape[0]):
+
+        x = X_new[i]
+
+        #x = phi(x)
+
+        k_mm = 0
+        for j in range(X.shape[0]):
+            k_mm += alphas[j] * kernel(X[j],x)
+
+        y_hat = k_mm
+
+        # print('y hat = {}'.format(y_hat))
+        # print('actual = {}'.format(Y[i]))
+
+        if y_hat * Y[i] < 0:  # wrong
+            num_wrong += 1
+        else:
+            num_right += 1
+
+    print('Num wrong = {}'.format(num_wrong))
+    print('Num right = {}'.format(num_right))
+    print('%Correct = {} %'.format(float(num_right) / (num_wrong + num_right)))
+
+if __name__ == '__main__':
+
+    print('Running kernalized perceptron')
+
+    ###################################################
+    # Generate a circle
+    ###################################################
+
+
+    run_create_circle_data = 1
+    if run_create_circle_data:
+        circle_y, circle_x = get_circle(2)
+        colors = ('blue', 'red')
+        groups = ('+', '-')
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(circle_x, circle_y, color='red', label='-')
+        x_p = multivariate_normal([0, 0], [[0.1, 0], [0, 0.1]], size=200)
+        x_p_1, x_p_2 = zip(*x_p)
+        ax.scatter(x_p_1, x_p_2, color='blue', label='+')
+        # ax.scatter(x_m_1,x_m_2,color='red',label='-')
+        plt.title('Data')
+        plt.legend()
+        plt.show()
+        x_m = np.array([circle_x, circle_y]).T
+
+
+    run_create_norm_data = 0
+    if run_create_norm_data:
+
+
+        #####################
+        # Generate some data
+        #####################
+        n_m = 100  # number minus
+        n_p = 100  # number plus
+        n = n_m + n_p
+        # set mean and covariance for both samples
+        mean_p = [2, 1]
+        cov_p = [[2, 0], [0, 2]]
+        mean_m = [-2, 1]
+        cov_m = [[2, 0], [0, 2]]
+
+        x_p = multivariate_normal(mean_p, cov_p, size=n_p)
+        x_m = multivariate_normal(mean_m, cov_m, size=n_m)
+
+        x_p_1, x_p_2 = zip(*x_p)
+        x_m_1, x_m_2 = zip(*x_m)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.scatter(x_p_1, x_p_2, color='blue', label='+')
+        ax.scatter(x_m_1,x_m_2,color='red',label='-')
+        plt.title('Data')
+        plt.legend()
+        plt.show()
+
+    X_data = np.concatenate((x_p, x_m))
+
+    Y_data = [1 for _ in range(len(x_p))] + [-1 for _ in range(len(x_m))]
+    Y_data = np.array(Y_data)
+
+    train_data_indexes = np.random.choice(len(X_data),int(len(X_data)*0.80), replace=False)
+    test_data_indexes = [i for i in range(len(X_data)) if i not in train_data_indexes]  # get whats left over for testing
+
+    train_data_x = X_data[train_data_indexes]
+    train_data_y = Y_data[train_data_indexes]
+
+    test_data_x = X_data[test_data_indexes]
+    test_data_y = Y_data[test_data_indexes]
+
+    alphas_trained = run_kernalized_perceptron(train_data_x, train_data_y,kernel_k1,phi=phi_I)
+
+    kernel_perceptron_sanity_check(test_data_x,train_data_x,test_data_y,alphas_trained,kernel_k1,phi=phi_I)
