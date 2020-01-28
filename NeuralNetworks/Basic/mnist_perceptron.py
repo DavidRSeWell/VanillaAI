7f8f1bc1b703cd30470bf729e3ee9311c7377157
David SeWell <davidsewell3145@gmail.com>
2020-01-28 06:38:38 -0800

    DS - Committing before implementing single batch code

--- NeuralNetworks/Basic/mnist_perceptron.py
+++ NeuralNetworks/Basic/mnist_perceptron.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import urllib
+import pandas as pd
+import seaborn as sns
 
-from sklearn import datasets
-from sklearn.datasets import fetch_mldata
 from shutil import copyfileobj
 from six.moves import urllib
 from sklearn.datasets.base import get_data_home
@@ -24,12 +24,16 @@ def fetch_mnist(data_home=None):
 
 class Perceptron:
 
-    def __init__(self,targets,weights=np.zeros((1)),data=None):
+    def __init__(self,training_data_x,training_data_y,
+                 test_data_x,test_data_y,weights=np.zeros(1)):
 
-        self.data = data
-        self.targets = targets
-        self.weights= weights
-        self.X = None
+        self.train_x = training_data_x
+        self.train_y = training_data_y
+
+        self.test_x = test_data_x
+        self.test_y = test_data_y
+
+        self.weights = weights
 
     def activation(self,y_hat):
 
@@ -43,85 +47,95 @@ class Perceptron:
 
         return y_hat
 
+    def confusion_matrix(self,targets):
+        '''
+        Build confusion matrix from test set
+        :return:
+        '''
+
+        Y = np.dot(self.test_x,self.weights)
+
+        Y_guess = np.argmax(Y,1)
+
+        con_mat = np.zeros((targets,targets))
+
+        for i in range(targets):
+            for j in range(targets):
+                con_mat[i,j] = np.sum(np.where(self.test_y == i,1,0)*np.where(Y_guess == j,1,0))
+
+        return con_mat
+
+
     def E_in(self):
         '''
         Compute total in sample error
         :return:
         '''
-        #error = 0
-        #for j in range(self.X.shape[0]):
 
-        #    x = self.X[j]
-
-        #    x = np.reshape(x,(1,self.X.shape[1]))
-
-        #    y_hat = self.forward(x)
-
-            #t_j = self.Y[j]
-
-        #    t_j = np.zeros(y_hat.shape)
-
-        #    t_j.T[int(self.Y[j])] = 1  # the jth indicator 1 or 0
-
-        #    error += np.abs(y_hat - t_j).sum()
-
-        #return error
-
-        Y = np.dot(self.X,self.weights)
+        Y = np.dot(self.train_x,self.weights)
 
         Y_guess = np.argmax(Y,1)
 
-        diff = self.Y - Y_guess
+        diff = self.train_y - Y_guess
+
+        diff = np.where(diff == 0.0,0,1)
+
+        num_wrong = diff.sum()
+
+        loss = (float(num_wrong) / self.train_y.shape[0])*100.0
+
+        correct = ((self.train_y.shape[0] - num_wrong) / float(self.train_y.shape[0]))*100.0
+
+        print('Train Num wrong = {}'.format(num_wrong))
+        print('% train num loss = {}'.format(loss))
+
+        return loss , correct
+
+    def E_out(self):
+        '''
+        Compute total in sample error
+        :return:
+        '''
+
+        Y = np.dot(self.test_x,self.weights)
+
+        Y_guess = np.argmax(Y,1)
+
+        diff = self.test_y - Y_guess
 
         diff = np.where(diff != 0,1,0)
 
         num_wrong = diff.sum()
 
-        loss = float(num_wrong) / self.Y.shape[0]
+        loss = (float(num_wrong) / self.test_y.shape[0])*100.0
 
-        print('Num wrong = {}'.format(num_wrong))
-        print('% loss = {}'.format(loss))
+        correct = ((self.test_y.shape[0] - num_wrong) / float(self.test_y.shape[0]))*100.0
 
-        #Y = self.activation(Y)
+        print('Test Num wrong = {}'.format(num_wrong))
+        print('% Test Num loss = {}'.format(loss))
 
-        #error = np.abs(Y - )
-
-        return loss
+        return loss , correct
 
     def forward(self,x):
 
-        N = x.shape[1]
-
-        #y_j = 0
-
-        #for i in range(N):  # loop over the input nodes
-
-        #    y_j += np.dot(self.weights[i][0], x[0][i])
-
         y_j = np.dot(x,self.weights)
-
-        #y_j = self.activation(y_j)
 
         y_j = np.where(y_j > 0,1,0)
 
         return y_j
 
-    def get_mse(self):
-
-        pass
-
     def init_weights(self,n):
         '''
         Function to initilize the set of weight and biases
         for the network
-        n = int. Length of input vector
+        n = int - Num targets
         :return:
         '''
-        W = np.random.random((n + 1,self.targets)) - 0.5
+        W = (np.random.random((self.train_x.shape[1],n)) - 0.5) / 100.0
 
         self.weights = W
 
-    def run_loop(self,data,labels,iters=100,alpha=0.025,threshold=0.5):
+    def run(self,iters=100,alpha=0.025):
         '''
         Naive means it is the most basic without any thought to optimization
         For iterations in iters
@@ -137,166 +151,123 @@ class Perceptron:
 
         print('Initializing weights')
 
-        N = data.shape[1] # Length of the feature vector
+        #N = data.shape[1] # Length of the feature vector
 
-        M = data.shape[0] # Length of the data set
+        M_train = self.train_x.shape[0] # Length of the data set
+        M_test = self.test_x.shape[0] # Length of the data set
 
-        self.X = np.concatenate((data.copy(), -1.0*np.ones((M,1))),axis=1)
+        self.train_x = np.concatenate((self.train_x.copy(), -1.0*np.ones((M_train,1))),axis=1)
+        self.test_x = np.concatenate((self.test_x.copy(), -1.0*np.ones((M_test,1))),axis=1)
 
-        self.Y = labels.copy()
+        targets = len(set(train_y))
 
-        self.init_weights(N)
+        train_targets = np.zeros((M_train,targets))
 
-        error = self.E_in()
+        index_array = [i for i in range(M_train)]
 
-        #print("Initial error {}".format(str(error)))
+        train_targets[index_array,self.train_y.astype(np.int)] = 1
 
-        for iter in range(iters):
+        self.init_weights(targets)
 
-            print('Running interation {}'.format(str(iter)))
+        train_error = self.E_in()
+        test_error = self.E_out()
 
-            for m in range(M): # loop over the data
+        train_errors = [train_error]
+        test_errors = [test_error]
 
-                x = self.X[m]
-
-                x = np.reshape(x,(1,N + 1))
-
-                y_hat = self.forward(x)
-
-                t_j = np.zeros(y_hat.shape)
-
-                t_j.T[int(self.Y[m])] = 1 # the jth indicator 1 or 0
-
-                # error
-                error = y_hat - t_j
-
-                # update weights
-                #for i in range(N + 1):
-
-                #    self.weights[i,0] -= alpha*(np.dot(error,x[0][i]))
-
-                self.weights -= alpha*(np.dot(x.T,error))
-
-            print('Error at iteration {} = {}'.format(str(iter),self.E_in()))
-
-        print('Final Weights')
-        #print(self.weights)
-
-    def run(self,data,labels,iters=100,alpha=0.025,threshold=0.5):
-        '''
-        Naive means it is the most basic without any thought to optimization
-        For iterations in iters
-            for data in data_set:
-                y = label
-                y_hat = prediction
-                error = error(y,y_hat)
-                update(error,weight)
-        :return:
-        '''
-
-        print('Running naive algorithm')
-
-        print('Initializing weights')
-
-        N = data.shape[1] # Length of the feature vector
-
-        M = data.shape[0] # Length of the data set
-
-        self.X = np.concatenate((data.copy(), -1.0*np.ones((M,1))),axis=1)
-
-        self.Y = labels.copy()
-
-        targets = np.zeros((M,self.targets))
-
-        index_array = [i for i in range(self.X.shape[0])]
-
-        targets[index_array,labels.astype(np.int)] = 1
-
-        self.init_weights(N)
-
-        error = self.E_in()
-
-        #print("Initial error {}".format(str(error)))
-
-        errors = [error]
+        train_corrects = []
+        test_corrects = []
 
         for iter in range(iters):
 
-            #print('Running interation {}'.format(str(iter)))
+            y_hat = self.forward(self.train_x)
 
-            y_hat = self.forward(self.X)
+            error = y_hat - train_targets
 
-            error = y_hat - targets
+            self.weights -= alpha*np.dot(self.train_x.T,error)
 
-            self.weights -= alpha*np.dot(self.X.T,error)
+            if (iter % 1 == 0):
 
-            '''
-            for m in range(M): # loop over the data
+                train_error,train_correct = self.E_in()
 
-                x = self.X[m]
+                test_error,test_correct = self.E_out()
 
-                x = np.reshape(x,(1,N + 1))
+                train_errors.append(train_error)
 
-                y_hat = self.forward(x)
+                test_errors.append(test_error)
 
-                t_j = np.zeros(y_hat.shape)
+                print('Error at iteration {} = {}'.format(str(iter),train_error))
 
-                t_j.T[int(self.Y[m])] = 1 # the jth indicator 1 or 0
+                train_corrects.append(train_correct)
 
-                # error
-                error = y_hat - t_j
+                test_corrects.append(test_correct)
 
-                # update weights
-                #for i in range(N + 1):
-
-                #    self.weights[i,0] -= alpha*(np.dot(error,x[0][i]))
-
-                self.weights -= alpha*(np.dot(x.T,error))
-            '''
-
-            if (iter % 10 == 0):
-                error = self.E_in()
-                errors.append(error)
-                print('Error at iteration {} = {}'.format(str(iter),error))
-
-        print('Final Weights')
-        #print(self.weights)
-
-        return errors
+        return train_errors, test_errors, train_corrects, test_corrects
 
 
 
 
 if __name__ == '__main__':
 
-    #mnist = datasets.load_digits()
-    #mnist = fetch_mldata('MNIST original') #data_home='/Users/befeltingu/PSUClasses/ML/')
 
     fetch_mnist()
     from sklearn.datasets import fetch_mldata
     mnist = fetch_mldata("MNIST original")
 
-    ###########################################################################
-    # the dataset contains a set of 150 records under five attributes -
-    # X = petal length, petal width, sepal length, sepal width
-    # Y (target) = species (setosa,versicolor,virginica )
-    ###########################################################################
+    ALPHA = 0.001
 
     mnist_data = mnist['data'].copy()
+    mnist_data = mnist_data.reshape((len(mnist_data), -1))
+
 
     mnist_data = mnist_data / 255.0
 
     mnist_targets = mnist['target'].copy()
 
-    mnist_data = mnist_data.reshape((len(mnist_data), -1))
+    train_data_indexes = np.random.choice(len(mnist_data), 60000, replace=False)
+    test_data_indexes = [i for i in range(len(mnist_data)) if
+                         i not in train_data_indexes]  # get whats left over for testing
 
-    percept = Perceptron(targets=10)
+    train_x = mnist_data[train_data_indexes]
+    train_y = mnist_targets[train_data_indexes]
 
-    errors = percept.run(mnist_data,mnist_targets,iters=500,alpha=0.001)
+    test_x = mnist_data[test_data_indexes]
+    test_y = mnist_targets[test_data_indexes]
 
-    plt.plot([i for i in range(len(errors))],errors)
+    percept = Perceptron(train_x,train_y,test_x,test_y)
+
+    train_errors, test_errors, train_correct, test_correct = percept.run(iters=500,alpha=ALPHA)
+
+    plt.plot([i for i in range(len(train_correct))],train_correct,label='training')
+
+    plt.plot([i for i in range(len(test_correct))],test_correct,label='test')
+
+    plt.legend()
+
+    plt.title('MNIST Experiment alpha = {} best test accuracy = {}'.format(ALPHA,np.max(test_correct)))
+    plt.ylabel('% correct')
+    plt.xlabel('% epoch')
 
     plt.show()
+
+    plt.close()
+
+    print('Building confusion matrix')
+    confusion_mat = percept.confusion_matrix(10)
+
+    conf_mat_df = pd.DataFrame(np.round(confusion_mat,5))
+
+    ax = sns.heatmap(conf_mat_df,annot=True,fmt='g')
+
+    bottom, top = ax.get_ylim()
+
+    ax.set_ylim(bottom + 0.5, top - 0.5)
+
+    plt.ylabel('Actuall Class')
+    plt.xlabel('Predicted Class')
+    plt.show()
+    plt.close()
+
 
     print('Done running mnist perceptron')
 
