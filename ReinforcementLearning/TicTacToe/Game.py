3275b5d2790513a424817d17c9a08752aa1aaff9
David SeWell <davidsewell3145@gmail.com>
2020-02-27 18:55:22 -0800

    DS - committing prior to changing the TicTacToe Q agent to just be a single player and then make the player as part of the state

--- ReinforcementLearning/TicTacToe/Game.py
+++ ReinforcementLearning/TicTacToe/Game.py
@@ -17,14 +17,27 @@ class RandomPlayer:
         '''
         Get all the possible moves that
         could be played from the current board
-        :return:
+        :return: list of integers
         '''
+
         legal_moves = []
+        i = 0
         for s in board.keys():
             if board[s] == ' ':
-                legal_moves.append(s)
+                legal_moves.append(i)
+            i += 1
 
         return legal_moves
+
+    def update(self,a,r,s):
+        '''
+
+        :param reward:
+        :param s1: current state
+        :param s2: next state
+        :return:
+        '''
+        pass
 
     def act(self,board):
         '''
@@ -36,7 +49,7 @@ class RandomPlayer:
 
         legal_moves = self.get_legal_moves(board)
 
-        return (self.side,np.random.choice(legal_moves))
+        return np.random.choice(legal_moves)
 
 class TabularQPlayer:
     '''
@@ -45,8 +58,11 @@ class TabularQPlayer:
 
     def __init__(self,side='X',epsilon=0.001):
 
+        self.alpha = 0.001
         self.current_state = None # varaible used for tracking state
         self.epsilon = epsilon
+        self.gamma = 0.1
+        self.pieces = {' ': 0, 'X': 1, 'O': -1} # used in converting the board dictionary to an array
         self.side = side
 
         self.Q = None # Q matrix with rows = states and columns = actions
@@ -64,6 +80,23 @@ class TabularQPlayer:
 
         return self.e_greedy(legal_moves)
 
+    def convert_board_to_array(self,s):
+        '''
+        Converts the board dictionary to
+        a numpy array
+        :param board_state: dictionary
+        :return:
+        '''
+
+        board = np.zeros((9,))
+        i = 0
+        for k, v in s.items():
+            board[i] = self.pieces[v]
+            i += 1
+
+        return board
+
+
     def e_greedy(self,moves):
         '''
         Take e-greedy action
@@ -71,12 +104,15 @@ class TabularQPlayer:
         :return:
         '''
 
+        s = self.convert_board_to_array(self.current_state)
+        s_index = self.get_state_index(s)
+
         if np.random.random() < self.epsilon:
             # take random action
             return np.random.choice(moves)
 
         else:
-            return moves[np.argmax(self.Q[moves])] # return the greedy action
+            return moves[np.argmax(self.Q[s_index][moves])] # return the greedy action
 
     def get_greedy(self,s):
         '''
@@ -113,6 +149,7 @@ class TabularQPlayer:
         :param s:
         :return:
         '''
+
         return np.where((self.S == s).all(axis=1))[0][0]
 
     def init(self):
@@ -129,14 +166,17 @@ class TabularQPlayer:
         :return:
         '''
 
-        s1_index = self.get_state_index(self.current_state)
+        s1 = self.convert_board_to_array(self.current_state)
 
-        s2_index = self.get_state_index(self.current_state)
-        s2 = self.S[s2_index]
+        s1_index = self.get_state_index(s1)
+
+        s2 = self.convert_board_to_array(s)
+
+        s2_index = self.get_state_index(s2)
 
         max_q_next = self.Q[s2_index][self.get_greedy(s2)]
 
-        self.Q[s1_index,a] = self.Q[s1_index,a] + self.alpha(r + self.gamma*max_q_next - self.Q[s1_index,a])
+        self.Q[s1_index,a] = self.Q[s1_index,a] + self.alpha*(r + self.gamma*max_q_next - self.Q[s1_index,a])
 
 class TicTacToe:
     '''
@@ -147,7 +187,7 @@ class TicTacToe:
     be bots or humans
     '''
 
-    def __init__(self,player1=None,player2=None):
+    def __init__(self,player1=None,player2=None,base_player=None):
 
         self.board = '''
                 | {s1} | {s2} | {s3} |    
@@ -187,24 +227,19 @@ class TicTacToe:
             board[i] = self.pieces[v]
             i += 1
 
-        return np.reshape(board, (3, 3))
+        return board
 
     def is_legal(self,action):
         '''
         Checks to make sure an action is legally possible
         :return:
         '''
-        player , coord = action
 
-        if self.game_state['player'].side != player:
-            print("Its not player {}'s turn".format(player))
-            return False
-        elif self.board_state[coord] != ' ': # if someone has already played there
-            print("Someone has already played {}".format(coord))
-            return False
-
-        elif coord not in self.board_state:
-            print("Coordinate {} is not on the board".format(coord))
+        legal_moves = np.where(self.convert_board_to_array() == 0.0)[0]
+        if action not in legal_moves:
+            print('Action {} is not legal action'.format(action))
+            print('CURRENT BOARD')
+            print(self)
             return False
 
         else:
@@ -231,6 +266,7 @@ class TicTacToe:
         '''
 
         board_mat = self.convert_board_to_array() # look at board as an array
+        board_mat = np.reshape(board_mat,(3,3))
 
         diag_1 = np.abs(sum([board_mat[i][i] for i in range(3)]))  # check diagnoal
         diag_2 = np.abs(sum([board_mat[i][3 - i - 1] for i in range(3)]))  # check other diagnol
@@ -248,71 +284,81 @@ class TicTacToe:
             return False
 
 
-    def run(self,show_board=True):
+    def run(self,iters=100,show_board=True):
         '''
         The main method used to simulate a single game
         :return:
         '''
 
-        self.player1.current_state = self.board_state
+        self.player1.current_state = self.board_state.copy()
 
-        self.player2.current_state = self.board_state
+        self.player2.current_state = self.board_state.copy()
 
-        while self.game_state['win_state'] == 0:
+        for iter in iters:
 
-            action = self.game_state['player'].act(self.board_state)
 
-            r = self.update(action)
+            while self.game_state['win_state'] == 0:
 
-            self.game_state['player'].update(action[1],r,self.board_state)
+                action = self.game_state['player'].act(self.board_state)
 
-            self.player1.current_state = self.board_state
+                r = self.update(action)
 
-            self.player2.current_state = self.board_state
+                if r != 0:
+                    pass
 
-            if show_board:
-                print(self)
+                self.game_state['player'].update(action,r,self.board_state)
+
+                self.player1.current_state = self.board_state.copy()
+
+                self.player2.current_state = self.board_state.copy()
+
+                # now update the current player
+                if self.game_state['player'] == self.player1:
+
+                    self.game_state['player'] = self.player2
+                else:
+
+                    self.game_state['player'] = self.player1
+
+                if show_board:
+                    print(self)
+
+        if (iter % 10) == 0:
+            # simulate 10 games vs random agent
+            pass
+
+    def simulate(self):
+
 
     def update(self,action):
         '''
         Take in an action from a player
         and update the game state
-        :param action: tuple (player, board coordinate) i.e. ('X', s1)
+        :param action: integer of action
         :return:
         '''
 
+        p_side = self.game_state['player'].side
         if self.is_legal(action):
+
+            board_action = list(self.board_state.keys())[action]
             # update the board
-            self.board_state[action[1]] = self.game_state['player'].side
+            self.board_state[board_action] = p_side
 
             # check if this is a winning move
             if self.is_win():
-                self.game_state['win_state'] = self.game_state['player'].side
+                self.game_state['win_state'] = p_side
                 return 1
 
             elif self.is_draw():
                 self.game_state['win_state'] = 2
                 return -1
 
-            # now update the current player
-            if self.game_state['player'] == self.player1:
-
-                self.game_state['player'] = self.player2
-            else:
-
-                self.game_state['player'] = self.player1
-
             return 0
 
         else:
-            print('The action {} by player {} not legal!!!'.format(action[0],action[1]))
+            print('The action {} by player {} not legal!!!'.format(p_side,action))
             return -1
-
-
-
-
-
-
 
 
 
@@ -320,9 +366,10 @@ if __name__ == '__main__':
 
     player1 = TabularQPlayer(side='X')
     player1.init()
-    player2 = RandomPlayer(side='O')
 
-    tic_tac_toe = TicTacToe(player1,player2)
+    base_player = RandomPlayer()
+
+    tic_tac_toe = TicTacToe(player1,base_player)
 
     tic_tac_toe.run()
 
